e0920c43f0468bc1f71f7e6f0c5c0bb9d257d271
Blake Moon <blake.moon@gmail.com>
2021-09-30 14:31:29 -0500

    Testing change for gh-pages action.

--- docs/build-docs-site.sh
+++ docs/build-docs-site.sh
@@ -13,8 +13,8 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
 # Python Sphinx, configured with source/conf.py
 # See https://www.sphinx-doc.org/
-make clean
-make html
+#make clean
+#make html
 
 #######################
 # Update GitHub Pages #
@@ -23,15 +23,20 @@ make html
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-docroot=`mktemp -d`
-mkdir -p "_build/html/"
-rsync -av "_build/html/" "${docroot}/"
-
-pushd "${docroot}"
+#docroot=`mktemp -d`
+#mkdir -p "_build/html/"
+#rsync -av "_build/html/" "${docroot}/"
+#
+#pushd "${docroot}"
 
 git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
+cd docs
+make clean
+make html
+
+
 
 # Adds .nojekyll file to the root to signal to GitHub that
 # directories that start with an underscore (_) can remain
